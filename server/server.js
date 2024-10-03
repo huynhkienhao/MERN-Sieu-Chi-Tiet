7bcf26455cf55e7a74307b29ee8a2b3396c22548
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-10-03 16:23:50 +0700

    video 5

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
-const port = 5000;
-
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+const port = 5000;
+
+// connect database
+mongoose.connect('mongodb://127.0.0.1:27017/mern_auth')
+    .then(() => {
+        console.log('Connect database success1');
+    })
+    .catch((err) => {
+        console.log(`Connect database fail ${err}`);
+    })
+
+
+// router for app
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World Nodejs');
 });
 
 app.get('/api/auth/login', (req, res) => {
