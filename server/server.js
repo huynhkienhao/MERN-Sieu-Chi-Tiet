cdf2e919cda845d8c2629c75e445dad69b23831f
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-10-06 15:30:07 +0700

    video 14

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const userRoute = require('./Router/UserRoute');
+const authRoute = require('./Router/AuthRoute');
 const connectDb = require('./Services/ConnectDbService');
 
 const port = 5000;
+
+// Middleware apply cors add all request
+app.use(cors());
 
 // connect database
 connectDb();
 
 // Middleware Router
 app.use('/users', userRoute);
+app.use('/api/auth', authRoute);
 
 
 app.listen(port, () => {
