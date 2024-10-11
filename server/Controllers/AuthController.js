3b6bb424ba8bdad0737eb9cfb75dd4704b532ccc
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-10-11 21:23:16 +0700

    video 21

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -18,6 +18,23 @@ const register = async (req, res) => {
     }
 }
 
+const login = async (req, res) => {
+    // Check email exit
+    const user = userModel.findOne({ email: req.body.email })
+    if (!user) {
+        return res.status(400).send('Invalid email or password (không hợp lệ)');
+    }
+
+    // Check password
+    const isPassValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!isPassValid) {
+        return res.status(400).send('Invalid email or password (không hợp lệ)');
+    }
+
+    return res.status(200).send('Login valid (hợp lệ)');
+}
+
 module.exports = {
-    register: register
+    register: register,
+    login: login
 }
