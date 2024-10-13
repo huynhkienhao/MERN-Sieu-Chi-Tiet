9aaa375ca579e4a43e5b7a2781317024295a2609
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-10-13 22:37:25 +0700

    video 30

--- client/js/admin_page.js
+++ client/js/admin_page.js
@@ -1,6 +1,18 @@
-// Call API get list users
-axios.get('http://localhost:5000/auth/admin/user', {
-    headers: {
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`
+
+getListUser = async () => {
+    try {
+        const configHeader = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`
+            }
+        }
+        // Call API get list users
+        const response = await axios.get('http://localhost:5000/auth/admin/user', configHeader);
+    } catch (error) {
+        // 1. Call refresh token
+        // 2. Token expired -> redirect to admin
     }
-})
+
+}
+
+getListUser();
