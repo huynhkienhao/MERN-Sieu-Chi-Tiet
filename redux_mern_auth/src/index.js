aab02ee96a784dbea1074ccea56835c1bdf27b3a
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-11-03 15:18:37 +0700

    video 57

--- redux_mern_auth/src/index.js
+++ redux_mern_auth/src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { combineReducers, createStore } from 'redux';
-import authReducer from './store/reducers/authReducer';
-import userReducer from './store/reducers/userReducer';
 import { Provider } from 'react-redux';
-
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const rootReducer = combineReducers({
-  auth: authReducer,
-  user: userReducer
-})
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 root.render(
   <Provider store={store}>
